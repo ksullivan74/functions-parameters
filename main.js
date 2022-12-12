3b0d227bba2822938893aa22a731ed801553d4a1
Neil Sullivan <ac30delay@gmail.com>
2022-12-12 09:56:05 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const quarterValue = (number) => 
+{
+    const quarter = number / 4
+    return quarter
+}
+
+// Invoke the function and store the return value
+const returnedNumber = quarterValue(50)
+
+// Log the return value
+
+console.log(returnedNumber)
+
+/*Define a parameter that will hold the value of the original number.
+Divide that number by 4
+Return the quartered number
+Store the returned number in a variable
+Plug that variable into the parenthesis for the console.log()
+When you run the code, you should see the answer in the console.
+*/
